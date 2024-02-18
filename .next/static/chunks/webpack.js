4bc9d87e3633d8ab9733cf82fb848f4a78e0c8a6
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2024-02-19 01:10:32 +0300

    **

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "790a188a22e3d2ca"; }
+/******/ 		__webpack_require__.h = function() { return "bc144a348144c932"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
